28a47fb8ed3f5781c2d3bdcf01354e1fa22712ca
kaspii314 <kate.stadelman@du.edu>
2022-01-22 22:43:10 -0600

    progress on cleaning up year columns

--- met_open_access.py
+++ met_open_access.py
@@ -2,6 +2,7 @@
 
 from ast import Assert
 import hashlib
+import numpy as np
 import pandas as pd
 import os
 import re
@@ -28,27 +29,51 @@ def get_data(source, outpath):
         except AssertionError:
             print('SHA256 Error:', source['checksum'], readable_hash)  
 
+def get_year(years, min_max='min'):
+    if len(years) == 0:                                                 # leave nulls alone
+        return ''
+    elif years.isnumeric():                                             # leave years that are already years alone
+        return years
+    elif re.match('[0-9]{4}-[0-9]{2}-[0-9]{2}', years):                 # YYYY-MM-DD -> YYYY
+        return years[0:4]
+    else:                                                               # year ranges
+        years_clean = re.sub('-', ' ', years)
+        years_lst = [ int(year) for year in years_clean.split() if year.isnumeric() ]
+        if min_max == 'min' and len(years_lst) > 0:
+            return(min(years_lst))
+        elif min_max == 'max' and len(years_lst) > 0:
+            return(max(years_lst))
+        else:
+            return ''
+
 def load_data(inpath, infile, sep=','):
     df = pd.read_csv(inpath + infile, sep=sep, low_memory=False)
-    # df_clean = df.apply(lambda x: re.sub('\s+',' ',str(x)) if x.dtype == "object" else x)       # remove extra spaces, newlines, tabs
-    # return df_clean
-    return df
+    df_clean = pd.DataFrame()
 
-def main(sources):
-    # define variables
-    data_path = os.path.join(os.getcwd(), 'data', '')
-    
+    # clean data
+    for column in df.columns:
+        df_clean[column] = df[column]
+        df_clean[column] = df_clean[column].apply(lambda x: str(x).strip())                  # remove leading and trailing whitespace
+        df_clean[column] = df_clean[column].apply(lambda x: re.sub('\s+',' ',str(x)))        # remove carriage returns, line feeds, and extra spaces
+        df_clean[column] = df_clean[column].apply(lambda x: str(x).replace('|','-'))         # consistent text separators for other data cleaning functions
+
+    # clean up year columns
+    df_clean['accession_year_clean'] = df_clean['AccessionYear'].apply(lambda x: str(x)[0:4])
+    df_clean['artist_begin_date_clean'] = df_clean['Artist Begin Date'].apply(lambda x: get_year(str(x), 'min'))
+    df_clean['artist_end_date_clean'] = df_clean['Artist End Date'].apply(lambda x: get_year(str(x), 'max'))
+
+    for column in df_clean.columns:
+        df_clean[column] = df_clean[column].apply(lambda x: str(x).replace('nan', ''))
+    df_clean.to_csv(inpath + 'MetObjects_clean.csv', index=False, sep='|')
+    return df_clean
+
+def main(data_path, sources):
     # # download data
     # for source in sources:
     #     get_data(source=sources[source], outpath=data_path)
 
     # load and clean data
     df = load_data(inpath=data_path, infile=sources['data']['file'])
-    test = df['Dimensions'].tolist()
-    print(test)
-
-    # s = 'ï»¿Object Number,Is Highlight,Is Timeline Work,Is Public Domain,Object ID,Gallery Number,Department,AccessionYear,Object Name,Title,Culture,Period,Dynasty,Reign,Portfolio,Constituent ID,Artist Role,Artist Prefix,Artist Display Name,Artist Display Bio,Artist Suffix,Artist Alpha Sort,Artist Nationality,Artist Begin Date,Artist End Date,Artist Gender,Artist ULAN URL,Artist Wikidata URL,Object Date,Object Begin Date,Object End Date,Medium,Dimensions,Credit Line,Geography Type,City,State,County,Country,Region,Subregion,Locale,Locus,Excavation,River,Classification,Rights and Reproduction,Link Resource,Object Wikidata URL,Metadata Date,Repository,Tags,Tags AAT URL,Tags Wikidata URL'
-    # print(s.encode('utf-8-sig'))
 
 if __name__ == '__main__':
     # Enforcing specific commit (cc6310135aeafd01fb03588929fb9d00c5cbc606) from Jan 17, 2022 because data is manually input with no tagged releases.
@@ -63,9 +88,14 @@ if __name__ == '__main__':
         }, 
         'data': { 
             'url': 'https://media.githubusercontent.com/media/metmuseum/openaccess/cc6310135aeafd01fb03588929fb9d00c5cbc606/', 
-            'file': 'MetObjects_test.csv',
+            'file': 'MetObjects.csv',
             'checksum': '68d26f76485ccd066d45b6044d0954f1508e431a21c4ef7133f45c46931d4773' 
         }
     }
 
-    main(sources=sources)
+    # Define where you want the data to live and mkdir if necessary
+    data_path = os.path.join(os.getcwd(), 'data', '')
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+
+    main(data_path=data_path, sources=sources)
